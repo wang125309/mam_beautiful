3978c3069b75501e09eb40696a3306353073ae4f
wangbailin <wangbailin@jiaoyin.cm>
2015-02-07 18:01:29 +0800

    first page + bonus

---
+++ static/js-modify/bonus.js
@@ -0,0 +1,21 @@
+require('../../bower_components/zepto/zepto.js');
+require('../../bower_components/zeptojs/src/touch.js');
+require('../../bower_components/velocity/velocity.js');
+window.onload = function() {
+	var coner_width = $(".coner").width();
+	var text_height = $(".bouns-center").height();
+	$(".coner").css({
+		"margin-top":-(coner_width-text_height)/2 + "px",
+		"margin-left":-coner_width/2 + "px"
+	});
+	bigger = 5;
+	$(".coner").velocity({
+		"height":2*bigger+coner_width + "px",
+		"width":2*bigger+coner_width + "px",
+		"margin-left":-(coner_width/2 + bigger) + "px",
+		"margin-top":-(coner_width-text_height + bigger)/2 + "px"
+	},{
+		"during":150,
+		"loop":true
+	});
+}
--- static/js-modify/index.js
+++ static/js-modify/index.js
@@ -1,1 +1,23 @@
 require('../../bower_components/zepto/zepto.js');
+require('../../bower_components/zeptojs/src/touch.js');
+require('../../bower_components/velocity/velocity.js');
+window.onload = function() {
+	var line_width = $(".finger-image").width();
+	$(".line").css("width",line_width+"px");
+	$(".line").css("margin-left","-"+line_width/2+"px");
+	$(".line").velocity({
+		height:line_width+"px"
+	},{
+		duration:800,
+		loop:true
+	});
+	$(".finger-area").tap(function(){
+		location.href="/bonus";
+	});
+	$(".instruct").tap(function(){
+		$(".instruction").velocity("fadeIn");
+	});
+	$(".instruction-close").tap(function(){
+		$(".instruction").velocity("fadeOut");
+	});
+};
