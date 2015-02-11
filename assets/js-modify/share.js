9ea2de1b7bd1d8b0a2a99d723225cc0c9ab28885
wangbailin <wangbailin@jiaoyin.cm>
2015-02-11 15:42:30 +0800

    fix

--- assets/js-modify/share.js
+++ assets/js-modify/share.js
@@ -8,14 +8,14 @@ $(function(){
 			$.get("/click/",function(data){
 				wx.onMenuShareTimeline({
 					title:'我是第'+data.num+'位加⼊入“和妈妈⼀起美丽下厨”的参与者,我为妈妈赢取六月鲜新年礼包',
-					link:'http://beauty.limijiaoyin.com/login/',
-					imgUrl:'http://beauty.limijiaoyin.com/static/image/share-center.png'
+					link:'http://slide.limijiaoyin.com/slides/mama#p0',
+					imgUrl:'http://beauty.limijiaoyin.com/static/image/share-center.png',
 				});
 				wx.onMenuShareAppMessage({
 					title:'和妈妈一起美丽下厨',
 					desc:'我是第'+data.num+'位加⼊入“和妈妈⼀起美丽下厨”⾏动的参与者,我为妈妈赢取六⽉月鲜新年礼包',
 					link:'http://beauty.limijiaoyin.com/login/',
-					imgUrl:'http://beauty.limijiaoyin.com/static/image/share-center.png'
+					imgUrl:'http://beauty.limijiaoyin.com/static/image/share-center.png',
 				});
 			});
 			wx.error(function(res){
@@ -28,14 +28,14 @@ $(function(){
 							$.get("/click/",function(data){
 								wx.onMenuShareTimeline({
 									title:'我是第'+data.num+'位加⼊入“和妈妈⼀起美丽下厨”的参与者,我为妈妈赢取六月鲜新年礼包',
-									link:'http://beauty.limijiaoyin.com/login/',
-									imgUrl:'http://beauty.limijiaoyin.com/static/image/share-center.png'
+									link:'http://slide.limijiaoyin.com/slides/mama#p0',
+									imgUrl:'http://beauty.limijiaoyin.com/static/image/share-center.png',
 								});
 								wx.onMenuShareAppMessage({
 									title:'和妈妈一起美丽下厨',
 									desc:'我是第'+data.num+'位加⼊入“和妈妈⼀起美丽下厨”⾏动的参与者,我为妈妈赢取六⽉月鲜新年礼包',
-									link:'http://beauty.limijiaoyin.com/login/',
-									imgUrl:'http://beauty.limijiaoyin.com/static/image/share-center.png'
+									link:'http://slide.limijiaoyin.com/slides/mama#p0',
+									imgUrl:'http://beauty.limijiaoyin.com/static/image/share-center.png',
 								});
 							
 							});	
