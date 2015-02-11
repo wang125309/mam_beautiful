880a43a12b240a35b863e16533a653cce010ae43
wangbailin <wangbailin@jiaoyin.cm>
2015-02-11 12:28:57 +0800

    sdf

--- portal/views.py
+++ portal/views.py
@@ -47,10 +47,11 @@ def update_access_token(request):
 		"status":"success"
 	})
 def bonus(request):
-	prize = bonus_get(request.session['openid'],request.session['nickname'])
+	#prize = bonus_get(request.session['openid'],request.session['nickname'])
 	cal = Calculate.objects.get(id=1)
 	cal.total = cal.total + 1
 	cal.save()
+	prize = "second"
 	return render(request,"bonus.html",{
 		"prize":prize,
 		"cal":cal.total
